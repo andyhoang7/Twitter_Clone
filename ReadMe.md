55eb2ed4ade8a899693341c40767ebb17aaa77c0
Thien Hoang <hoangkimthien@gmail.com>
2019-08-01 16:07:19 +0700

    up 1

---
+++ ReadMe.md
@@ -0,0 +1,79 @@
+# CoderSchool FTW - * Twitter_CoderSchool *
+
+Created with love by: <Ms. Phuong, Mr. Khuong, Mr. Hang, Mr. Thien>
+  
+One or two sentence summary of your project. 
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories.
+
+To create a GIF, use [LiceCap](http://www.cockos.com/licecap/), [RecordIt](http://www.recordit.co), or [Loom](http://www.useloom.com), and link the image here in the markdown.
+
+```
+<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+```
+
+## User Stories
+
+The following **required** functionalities are completed (this is a sample):
+K P H T
+<!-- Team -->
+* [x] The user should be able to enter a message into a text field. 
+* [x] The user should be able to press "Tweet" and see the message pop in below the text box.
+* [x] The user should be able to delete a tweet.
+* [x]/ Add favicon to the page
+
+<!--  -->
+* [] The user should see a "count" that displays how many more characters the user has remaining, counting down from 140.
+* [] Upon tweeting, the characters remaining text should reset back to 140, and the field should be cleared.
+* [] The application should disallow text of greater than 140 characters.
+
+<!--  -->
+* [] The user should be able to "Like". When "Like" is clicked, the text should change to "Unlike". When "Unlike" is clicked, the text should change to "Like". Alternatively, use a heart icon (colored or not colored).
+* [] When a "Liked" tweet is retweeted, the new tweet should not be liked.
+
+<!--  -->
+* [] Each tweet has an optional hashtag. The hashtag should be linked with an anchor tag.
+* []/ The user can click on a hashtag, and only other tweets with that hashtag are shown.
+
+<!--  -->
+* [] The user should be able to "Retweet". Clicking Retweet immediately inserts a copy of that tweet below the original tweet.
+* []/ When the user deletes a tweet, all retweets should be deleted.
+
+
+
+
+The following **optional** features are implemented:
+
+
+* [] Download and upload the list of tweets using the myjson api, as shown in class.
+* [] Users can mention other users. Any string that starts with @username will be highlighted in blue.
+
+* [] Any tweet with an image URL will have the image automatically expanded in the tweet.
+
+The following **additional** features are implemented:
+
+* [] List anything else that you can get done to improve the page!
+
+## Time Spent and Lessons Learned
+
+Time spent: **X** hours spent in total.
+
+Describe any challenges encountered while building the app.
+
+## License
+
+    Copyright [yyyy] [name of copyright owner]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ js/scripts.js
@@ -0,0 +1,146 @@
+console.log("Twitter-Clone");
+
+document.getElementById("userInput").focus();
+
+var input = document.getElementById("userInput");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    addTweet();
+  }
+});
+
+// const tweets = [
+//   {
+//     body: "This is Thien  post signed in!",
+//     likeCount: 0,
+//    reTweets: [],
+//     createdAt: "2019-08-01T04:02:07.096Z",
+//     userName: "datloiboi@gmail.com"
+//   },
+//   {
+//     body: "This is Phuong  post signed in!",
+//     likeCount: 0,
+//    reTweets: [],
+//     createdAt: "2019-08-01T04:02:07.096Z",
+//     userName: "datloiboi@gmail.com"
+//   },
+//   {
+//     body: "This is Hang  post signed in!",
+//     likeCount: 0,
+//    reTweets: [],
+//     createdAt: "2019-08-01T04:02:07.096Z",
+//     userName: "datloiboi@gmail.com"
+//   }
+// ];
+
+let tweets = JSON.parse(localStorage.getItem("tweets")) || [];
+
+function createTwitterObject() {
+  return {
+    body: document.getElementById("userInput").value,
+    likeCount: 0,
+    retweets: [1,2,3,4],
+    createdAt: new Date(),
+    userName: "New User"
+  };
+  
+  // newTweet.body = document.getElementById("userInput").value;
+  // new
+  //   console.log("line 36 ", newTweet);
+}
+
+function storeTweetsToLocalStorage(tweets){
+  localStorage.setItem("tweets", JSON.stringify(tweets));
+}
+
+function addTweet() {
+  newTweet = createTwitterObject();
+  tweets.unshift(newTweet);
+  console.log("", newTweet);
+  renderTweets(tweets);
+  document.getElementById("userInput").focus();
+  document.getElementById("userInput").value = "";
+  storeTweetsToLocalStorage(tweets);
+}
+function removeTweet(idx) {
+  console.log("user delete tweets number", idx);
+  tweets.splice(idx, 1);
+  renderTweets(tweets);
+  storeTweetsToLocalStorage(tweets);
+}
+
+function renderTweets(tweets) {
+  const liHtmls = tweets.map((el, idx) => {
+    return createtweetHtml(el, idx);
+  });
+  document.getElementById("tweetList").innerHTML = liHtmls.join("");
+}
+
+function createtweetHtml(el, idx) {
+  return ` <div class="post-bar">
+<div class="post_topbar">
+  <div class="usy-dt">
+    <img src="images/resources/us-pic.png" alt="">
+    <div class="usy-name">
+      <h3>${el.userName}</h3>
+      <span><img src="images/clock.png" alt="">${el.createdAT}</span>
+    </div>
+  </div>
+  <div class="ed-opts">
+    <a href="#" title="" class="ed-opts-open"><i class="la la-ellipsis-v"></i></a>
+    <ul class="ed-options">
+      <li><a href="#" title="">Edit Post</a></li>
+      <li><a href="#" title="">Unsaved</a></li>
+      <li><a href="#" title="">Unbid</a></li>
+      <li><a href="#" title="">Close</a></li>
+      <li><a href="#" title="">Hide</a></li>
+    </ul>
+  </div>
+</div>
+<div class="epi-sec">
+  <ul class="descp">
+    <li><img src="images/icon8.png" alt=""><span>Epic Coder</span></li>
+    <li><img src="images/icon9.png" alt=""><span>India</span></li>
+  </ul>
+  <ul class="bk-links">
+    <li><a href="#" title=""><i class="la la-bookmark"></i></a></li>
+    <li><a href="#" title=""><i class="la la-envelope"></i></a></li>
+  </ul>
+</div>
+<div class="job_descp">
+
+  <p>${el.body}</p>
+  <ul class="skill-tags">
+    <li><a href="#" title="">HTML</a></li>
+    <li><a href="#" title="">PHP</a></li>
+    <li><a href="#" title="">CSS</a></li>
+    <li><a href="#" title="">Javascript</a></li>
+    <li><a href="#" title="">Wordpress</a></li> 	
+  </ul>
+</div>
+<div class="job-status-bar">
+  <ul class="like-com">
+    <li>
+      <a href="#"><i class="la la-heart"></i> Like</a>
+      <img src="images/liked-img.png" alt="">
+      <span>${el.likeCount}</span>
+    </li> 
+    <li><a href="#" title="" class="com"><img src="images/com.png" alt="">Retweets: ${
+      el.retweetCount
+    }</a></li>
+  </ul>
+  <a onclick="removeTweet(${idx})"><i class="la la-eye"></i>Delete</a>
+  ${renderComments(el.retweets)}
+</div>
+</div> `;
+}
+
+
+
+function renderComments(retweets) {
+  return retweets.map((retweet) => {
+    return `<h1>I'm a retweet</h1>`
+  }).join('')
+
+}
+// renderTweets(tweets);
