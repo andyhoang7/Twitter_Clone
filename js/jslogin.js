5036955c36f9422deca69bee9993793463c3052b
MorsChen <MorsChenArtist@users.noreply.github.com>
2019-08-02 10:25:08 +0700

    Update Login Full Fri

--- js/jslogin.js
+++ js/jslogin.js
@@ -2,7 +2,6 @@ var count = 2;
 
 let usr = JSON.parse(localStorage.getItem("ip01"));
 
-<<<<<<< HEAD
 // var npwd = document.getElementById("ip001").value;
 // var rpwd = document.getElementById("ip002").value;
 var usernameArray = ["Mors", "Phuong", "Khuong", "Thien"];
@@ -20,25 +19,6 @@ var passwordArray = ["01234", "43210", "56789", "98765"];
 //         }
 //     })
 // }
-=======
-var npwd = document.getElementById("ip001").value;
-var rpwd = document.getElementById("ip002").value;
-var usernameArray = ["Mors", "Phuong", "Khuong", "Thien"];
-var passwordArray = ["01234", "43210", "56789", "98765"];
-
-function storeUserToStorage(usr) {
-    localStorage.setItem("ip01",JSON.stringify(usr))
-}
-
-function storePWToStorage(pwd) {
-    localStorage.setItem("ip001",JSON.stringify(pwd) => {
-        if (npwd = rpwd) {
-            let npwd = JSON.parse(localStorage.getItem("ip001"))
-            
-        }
-    })
-}
->>>>>>> 5b40b9a2bc2c14c257c16bfdb549799ca705af16
 
 
 
@@ -77,7 +57,6 @@ function validate() {
   }
 }
 function showPassword() {
-<<<<<<< HEAD
     var x = document.getElementById("ip001");
     if (x.type === "password") {
       x.type = "text";
@@ -85,15 +64,6 @@ function showPassword() {
       x.type = "password";
     }
   }
-=======
-  var x = document.getElementById("ip001");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
-}
->>>>>>> 5b40b9a2bc2c14c257c16bfdb549799ca705af16
 
 function showRePassword() {
   var x = document.getElementById("ip002");
@@ -102,8 +72,4 @@ function showRePassword() {
   } else {
     x.type = "password";
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 5b40b9a2bc2c14c257c16bfdb549799ca705af16
